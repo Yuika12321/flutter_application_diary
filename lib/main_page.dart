d2bfa0273d9ab6aa2a0c33476e460ca59894fdd0
Yuika12321 <131143940+Yuika12321@users.noreply.github.com>
2023-09-12 17:00:17 +0900

    Update main_page.dart

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_application_diary/add_page.dart';
+import 'package:flutter_diary/add_page.dart';
+import 'package:path_provider/path_provider.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -27,23 +30,43 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
+  late Directory? directory;
+  String filePath = '';
+  @override
+  void initState() async {
+    // TODO: implement initState
+    super.initState();
+    getPath();
+  }
+
+  Future<void> getPath() async {
+    directory = await getApplicationSupportDirectory();
+    if (directory != null) {
+      var fileName = 'diary.json';
+      filePath = '${directory!.path}/$fileName'; // 경로/경로/diary.json
+      print(filePath);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Text('asdf'),
+      body: const Text('hello'),
       floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            var result = await Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => AddPage(filePath: 'temp'),
+        onPressed: () async {
+          var result = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddPage(
+                filePath: 'temp',
               ),
-            );
-            print(result);
-          },
-          child: const Icon(
-            Icons.attachment,
-          )),
+            ),
+          );
+
+          print(result);
+        },
+        child: const Icon(Icons.pest_control_outlined),
+      ),
     );
   }
 }
